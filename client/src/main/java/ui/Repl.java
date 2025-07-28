4dd90df3267d85cc60dda1ae424a979a39f781bd
ldsryush <ldsryush@gmail.com>
2025-07-28 00:19:30 -0600

    created state and repl

---
+++ client/src/main/java/ui/Repl.java
@@ -0,0 +1,41 @@
+package ui;
+
+
+import java.util.Scanner;
+
+import static ui.EscapeSequences.*;
+
+public class Repl {
+    private final Client client;
+
+    public Repl(String serverUrl) {
+        client = new Client(serverUrl, this);
+    }
+
+    public void run() {
+        System.out.println("👑 Welcome to 240 Chess. Type Help to get started. 👑");
+        System.out.print(client.help());
+
+        Scanner scanner = new Scanner(System.in);
+        var result = "";
+
+        while (!result.equals("quit")) {
+            printPrompt();
+            String line = scanner.nextLine();
+
+            try {
+                result = client.eval(line);
+                System.out.print(SET_TEXT_COLOR_BLUE + result);
+            } catch (Throwable e) {
+                var msg = e.toString();
+                System.out.print(msg);
+            }
+        }
+
+        System.out.println();
+    }
+
+    private void printPrompt() {
+        System.out.print("\n" + RESET_BG_COLOR + ">>> " + SET_TEXT_COLOR_GREEN);
+    }
+}
