2487ef9386a4e7548bf770450b9de84bc60f4aa7
ldsryush <ldsryush@gmail.com>
2025-08-07 12:53:53 -0600

    deleted unused NotificationHandler

--- server/src/main/java/websocket/NotificationHandler.java
+++ server/src/main/java/websocket/NotificationHandler.java
@@ -7,12 +7,11 @@ import websocket.messages.NotificationMessage;
 public interface NotificationHandler {
 
     // Send to one client
-    void notify(ClientConnection recipient, NotificationMessage message);
     void loadGame(ClientConnection recipient, LoadGameMessage message);
     void error(ClientConnection recipient, ErrorMessage message);
 
     // Broadcast to others in the same game
-    void notifyOthers(ClientConnection sender, NotificationMessage message); // ✅ Added this
+    void notifyOthers(ClientConnection sender, NotificationMessage message);
     void notifyOthers(ClientConnection sender, LoadGameMessage message);
 
     // Broadcast to all in the game
